9ac5f89b128d30f19329debf2a68096e01e3d3d3
chinthanah <chinthanahssuresh@gmail.com>
2023-09-16 10:41:20 +0530

    Created class based components:navbar,news,newsItem

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import "./App.css";
 import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
 
 export default class App extends Component {
   render() {
-    return <div>Hello my first class based component</div>;
+    return (
+      <div>
+        <Navbar />
+        <News />
+      </div>
+    );
   }
 }
