b975e61a93bff844c27cb0e7c42b2760c35a03b2
chinthanah <chinthanahssuresh@gmail.com>
2023-09-17 19:23:44 +0530

    added news API

--- src/components/News.js
+++ src/components/News.js
@@ -4,13 +4,19 @@ import NewsItem from "./NewsItem";
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is a news component
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
+      <div className="container">
+        <h2>NEWS Top Headlines</h2>
+        <div className="row">
+          <div className="col-md-4">
+            <NewsItem title="mytitle" description="mydesc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="mytitle" description="mydesc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="mytitle" description="mydesc" />
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,7 +2,25 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    return <div>This is a newsItem componenet</div>;
+    let { title, description } = this.props;
+    return (
+      <div>
+        <div class="card" style={{ width: "18rem" }}>
+          <img
+            src="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"
+            class="card-img-top"
+            alt="..."
+          />
+          <div class="card-body">
+            <h5 class="card-title">{title}</h5>
+            <p class="card-text">{description}</p>
+            <a href="#" class="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
