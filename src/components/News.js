f65e5267bf426f396f3b9574e39b03592e87267d
chinthanah <chinthanahssuresh@gmail.com>
2023-09-17 19:59:45 +0530

    added image

--- src/components/News.js
+++ src/components/News.js
@@ -6,15 +6,27 @@ export class News extends Component {
     return (
       <div className="container">
         <h2>NEWS Top Headlines</h2>
-        <div className="row">
+        <div className="row my-2">
           <div className="col-md-4">
-            <NewsItem title="mytitle" description="mydesc" />
+            <NewsItem
+              title="mytitle"
+              description="mydesc"
+              imageUrl="https://www.aljazeera.com/wp-content/uploads/2023/09/33V82NE-highres-1694809059.jpg"
+            />
           </div>
           <div className="col-md-4">
-            <NewsItem title="mytitle" description="mydesc" />
+            <NewsItem
+              title="mytitle"
+              description="mydesc"
+              imageUrl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"
+            />
           </div>
           <div className="col-md-4">
-            <NewsItem title="mytitle" description="mydesc" />
+            <NewsItem
+              title="mytitle"
+              description="mydesc"
+              imageUrl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg"
+            />
           </div>
         </div>
       </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,15 +2,11 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description } = this.props;
+    let { title, description, imageUrl } = this.props;
     return (
       <div>
         <div class="card" style={{ width: "18rem" }}>
-          <img
-            src="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"
-            class="card-img-top"
-            alt="..."
-          />
+          <img src={imageUrl} />
           <div class="card-body">
             <h5 class="card-title">{title}</h5>
             <p class="card-text">{description}</p>
