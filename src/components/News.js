0bc61400962687eb6785c14d4be9ed1d069ebfa5
chinthanah <chinthanahssuresh@gmail.com>
2023-09-18 20:41:32 +0530

    added previous and next button

--- src/components/News.js
+++ src/components/News.js
@@ -2,82 +2,61 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export class News extends Component {
-  articles = [
-    {
-      source: {
-        id: "al-jazeera-english",
-        name: "Al Jazeera English",
-      },
-      author: "Hafsa Adil",
-      title:
-        "‘Every day is a struggle’: Sri Lanka banks on Asia Cup cricket tournament",
-      description:
-        "Cricket is bringing temporary relief to a struggling Sri Lankan economy reeling from post-COVID shortages and inflation.",
-      url: "http://www.aljazeera.com/sports/2023/9/16/sri-lanka-banks-on-asia-cup-cricket-tournament",
-      urlToImage:
-        "https://www.aljazeera.com/wp-content/uploads/2023/09/33V82NE-highres-1694809059.jpg?resize=1920%2C1440",
-      publishedAt: "2023-09-16T09:42:46Z",
-      content:
-        "Colombo, Sri Lanka Until a few months ago, SMS Senaratne struggled to put three meals on the table for his family.\r\nWhen the economic crisis struck the country two years ago it took away our savings,… [+4157 chars]",
-    },
-    {
-      source: {
-        id: "espn-cric-info",
-        name: "ESPN Cric Info",
-      },
-      author: null,
-      title:
-        "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-      description:
-        "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-      url: "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-      urlToImage:
-        "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-      publishedAt: "2020-04-27T11:41:47Z",
-      content:
-        "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]",
-    },
-    {
-      source: {
-        id: "espn-cric-info",
-        name: "ESPN Cric Info",
-      },
-      author: null,
-      title:
-        "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-      description:
-        "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-      url: "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-      urlToImage:
-        "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-      publishedAt: "2020-03-30T15:26:05Z",
-      content:
-        "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]",
-    },
-  ];
   constructor() {
     super();
     console.log("Hello I'm a constructor");
     this.state = {
-      articles: this.articles,
+      articles: [],
       loading: false,
+      page: 1,
     };
   }
 
   async componentDidMount() {
     let url =
-      "https://newsapi.org/v2/top-headlines?country=in&apiKey=7722231d02234b0ab37ed6574ce60572";
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=7722231d02234b0ab37ed6574ce60572&page=1&pageSize=10";
     let data = await fetch(url);
     let parsedData = await data.json();
     console.log(parsedData);
     this.setState({ articles: parsedData.articles });
   }
 
+  handlePrevClick = async () => {
+    console.log("previous");
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=7722231d02234b0ab37ed6574ce60572&page=${
+      this.state.page - 1
+    }&pageSize=10`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+    });
+  };
+
+  handleNextClick = async () => {
+    console.log("next");
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=7722231d02234b0ab37ed6574ce60572&page=${
+        this.state.page + 1
+      }&pageSize=10`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      console.log(parsedData);
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+      });
+    }
+  };
+
   render() {
     console.log("render");
     return (
       <div className="container my-3">
-        <h2>NEWS Top Headlines</h2>
+        <h1>NEWS Top Headlines</h1>
         <div className="row my-2">
           {this.state.articles.map((element) => {
             return (
@@ -91,6 +70,23 @@ export class News extends Component {
               </div>
             );
           })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            class="btn btn-dark"
+            onClick={this.handlePrevClick}
+          >
+            &larr; Previous
+          </button>
+          <button
+            type="button"
+            class="btn btn-dark"
+            onClick={this.handleNextClick}
+          >
+            &rarr; Next
+          </button>
         </div>
       </div>
     );
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -10,7 +10,12 @@ export class NewsItem extends Component {
           <div class="card-body">
             <h5 class="card-title">{title}</h5>
             <p class="card-text">{description}</p>
-            <a href={newsUrl} target="_blank" class="btn btn-sm btn-primary">
+            <a
+              rel="noreferrer"
+              href={newsUrl}
+              target="_blank"
+              class="btn btn-sm btn-dark"
+            >
               Read more
             </a>
           </div>
