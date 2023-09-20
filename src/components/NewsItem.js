164411aaee6cf726962d069008524509442f0334
chinthanah <chinthanahssuresh@gmail.com>
2023-09-20 11:10:21 +0530

    added author, date and badge

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,14 +2,29 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description, imageUrl, newsUrl } = this.props;
+    let { title, description, imageUrl, newsUrl, author, date, source } =
+      this.props;
     return (
       <div className="my-3">
         <div class="card">
+          <span
+            class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
+            style={{ zIndex: 1 }}
+          >
+            {source}
+            <span class="visually-hidden">unread messages</span>
+          </span>
           <img src={imageUrl} className="card-img-top" alt="..." />
           <div class="card-body">
             <h5 class="card-title">{title}</h5>
             <p class="card-text">{description}</p>
+            <p class="card-text">
+              <small class="text-body-secondary">
+                By {!author ? "Unknown" : author} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+
             <a
               rel="noreferrer"
               href={newsUrl}
